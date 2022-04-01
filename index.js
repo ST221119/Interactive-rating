b7e48302fe384ac4a69ffa68a3ec9773575a8092
ST221119 <95220929+ST221119@users.noreply.github.com>
2022-04-01 22:13:36 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function firstFunction(){
+  document.getElementsByClassName("button")[0].style.color="black";
+  document.getElementsByClassName("button")[0].style.backgroundColor="hsl(217, 12%, 63%)";
+  document.querySelector("span").innerHTML="1";
+}
+
+function secondFunction(){
+  document.getElementsByClassName("button")[1].style.color="black";
+  document.getElementsByClassName("button")[1].style.backgroundColor="hsl(217, 12%, 63%)";
+  document.querySelector("span").innerHTML="2";
+}
+
+function thirdFunction(){
+  document.getElementsByClassName("button")[2].style.color="black";
+  document.getElementsByClassName("button")[2].style.backgroundColor="hsl(217, 12%, 63%)";
+  document.querySelector("span").innerHTML="3";
+}
+
+function fourthFunction(){
+  document.getElementsByClassName("button")[3].style.color="black";
+  document.getElementsByClassName("button")[3].style.backgroundColor="hsl(217, 12%, 63%)";
+  document.querySelector("span").innerHTML="4";
+}
+
+function fifthFunction(){
+  document.getElementsByClassName("button")[4].style.color="black";
+  document.getElementsByClassName("button")[4].style.backgroundColor="hsl(25, 97%, 53%)";
+  document.querySelector("span").innerHTML="5";
+}
+
+function lastFunction(){
+  document.querySelector(".submit").style.color="hsl(25, 97%, 53%)";
+  document.querySelector(".submit").style.backgroundColor="hsl(0, 0%, 100%)";
+  document.getElementById('first-div').style.display="none";
+  document.getElementById('second-div').style.display="block";
+}
